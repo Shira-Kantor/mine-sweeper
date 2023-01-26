674a9ee0e506d5fa7431ccf4807acceb0a0098e8
Shira-Kantor <123474518+Shira-Kantor@users.noreply.github.com>
2023-01-26 16:14:55 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,22 +7,29 @@ const EMPTY = ''
 var gBoard
 var gLevel = { size: 4, mines: 2 }
 var gGame = { isOn: false, shownCount: 0, markedCount: 0, secsPassed: 0 }
+var cellCount
 
 
 
 function onInit() {
+    cellCount = 0
+    gGame.isOn = true
+    closeModal()
+    clearInterval(timerInterval)
+
+
+}
+function onStart() {
+    gGame.isOn = true
     gBoard = buildBoard(4)
-    addMine()
+    addMine(2)
     renderBoard(gBoard)
     updateMinesAroundCount()
-
+    gGame.secsPassed = timer()
 }
 
-function OnCell() {
+function buildBoard(size) {
 
-}
-function buildBoard() {
-    var size = gLevel.size
     var board = []
     for (var i = 0; i < size; i++) {
         board[i] = []
@@ -34,48 +41,106 @@ function buildBoard() {
     return board
 }
 
+function changeLevel(size) {
+    document.querySelector(".easy-level").addEventListener("click",
+        function () { changeLevel(4) });
+    document.querySelector(".medium-level").addEventListener("click",
+        function () { changeLevel(8) });
+    document.querySelector(".hard-level").addEventListener("click",
+        function () { changeLevel(12) });
+    gGame.isOn = true
+    clearInterval(timerInterval)
+    gBoard = buildBoard(size)
+    renderBoard(gBoard)
+    if (size === 8) addMine(14)
+    if (size === 12) addMine(32)
+
+
+}
+
 function renderCell(location, value) {
-    
+
     const elCell = document.querySelector(`cell cell-${location.i}-${location.j}`)
-    console.log('elCell',elCell)
+    console.log('elCell', elCell)
 
     elCell.innerHTML = value
 }
 
 function renderBoard(board) {
-    console.log('board', board)
-    var strHTML = ''
-    for (var i = 0; i < board.length; i++) {
-        strHTML += '<tr>'
-        for (var j = 0; j < board[0].length; j++) {
-            var currCell = board[i][j]
-            var className = `cell cell-${i}-${j}`
-            // currCell = (currCell.isMine) ? BOOM : currCell.minesAroundCount
-            currCell = ''
-            strHTML += `<td class="${className}" onclick="onCellClicked(this,${i},${j})" >`
-            strHTML += currCell
+    if (gGame.isOn) {
+        // console.log('board', board)
+        var strHTML = ''
+        for (var i = 0; i < board.length; i++) {
+            strHTML += '<tr>'
+            for (var j = 0; j < board[0].length; j++) {
+                var currCell = board[i][j]
+                var className = `cell cell-${i}-${j}`
+                currCell = ''
+                strHTML += `<td class="${className}"  onmousedown="handleClick(event,this,${i},${j})">`
+                strHTML += currCell
 
-            strHTML += '</td>'
+                strHTML += '</td>'
+            }
+            strHTML += '</tr>'
+
         }
-        strHTML += '</tr>'
-
+        const elBoard = document.querySelector('.board')
+        elBoard.innerHTML = strHTML
+        var cells = document.querySelectorAll('.cell')
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].addEventListener("click", function (e) {
+                handleClick(e, cells[i], cells[i].classList[1].split("-")[1], cells[i].classList[1])
+            })
+        }
     }
-    const elBoard = document.querySelector('.board')
-    elBoard.innerHTML = strHTML
 }
 
-// const myButton = document.querySelector(`cell cell-${0}-${0}`);
-// myButton.addEventListener("click", function(event) {
-//   if (event.button === 0) {
-//     console.log("Left button clicked");
-//   } else if (event.button === 2) {
-//     console.log("Right button clicked");
-//   }
-// })
+function handleClick(event, element, i, j) {
+
+    if (event.button === 0) {
+        onCellClicked(element, i, j, gBoard)
+        // console.log("Left button clicked on render board.");
+    }
+    var rendringCell = document.querySelector(`.board`);
+    rendringCell.addEventListener("contextmenu", function (event) {
+        event.preventDefault();
+        var elCell = event.target;
+        onCellMarked(elCell)
+        // console.log("Right button clicked on render board.");
+    })
+}
+
+function onCellMarked(elCell) {
+    event.preventDefault()
+
+    if (elCell) {
+        // console.log('elCell', elCell);
+        elCell.innerText = FLAG;
+        cellCount++
+    } else {
+        console.log("Invalid element");
+    }
+
+}
+
+function onCellClicked(elCell, cellI, cellJ, board) {
+    // console.log('gBoard[cellI][cellJ].isMine', gBoard[cellI][cellJ].isMine)
+    // gBoard[cellI][cellJ].isShown =true
+    if (gBoard[cellI][cellJ].isMine) {
+        elCell.innerText = BOOM
+        gameOver()
+    } else {
+        elCell.innerText = gBoard[cellI][cellJ].minesAroundCount
+        cellCount++
+        if (gBoard[cellI][cellJ].minesAroundCount === 0) {
+            elCell.innerText = ''
 
 
-// (currCell.isMine) ? BOOM : currCell.minesAroundCount
+            expanShown(gBoard, elCell, cellI, cellJ)
 
+        }
+    }
+}
 function getEmptyPos() {
     const emptyPoss = []
     for (var i = 0; i < gBoard.length; i++) {
@@ -89,72 +154,56 @@ function getEmptyPos() {
     return emptyPoss[randIdx]
 }
 
-function addMine() {
+function addMine(num) {
 
-    var num = gLevel.mines
-    for (var i = 0; i < num; i++) {
+    // num = gLevel.mines
+    for (var m = 0; m < num; m++) {
         var emptyPoss = getEmptyPos()
         gBoard[emptyPoss.i][emptyPoss.j].isMine = true
 
     }
+    // renderBoard(gBoard)
 }
 
-function onCellClicked(elCell, cellI, cellJ) {
-    console.log(gBoard[cellI][cellJ].isMine);
-
-    if (gBoard[cellI][cellJ].isMine) {
-        elCell.innerText = BOOM
-        gameOver()
-    } else {
-        elCell.innerText = gBoard[cellI][cellJ].minesAroundCount
-        if (gBoard[cellI][cellJ].minesAroundCount === 0) {
-            elCell.innerText = ''
-            expanShown(gBoard, elCell, cellI, cellJ)
-
-        }
-    }
-
-
-}
 function expanShown(board, elCell, cellI, cellJ) {
+
+    // console.log('gBoard[i][j].minesAroundCount',gBoard[i][j].minesAroundCount)
+    // if (gBoard[cellI][cellJ].minesAroundCount === 0) {
     for (var i = cellI - 1; i <= cellI + 1; i++) {
         if (i < 0 || i >= board.length) continue
         for (var j = cellJ - 1; j <= cellJ + 1; j++) {
             if (j < 0 || j >= board[i].length) continue
-            if (i === cellI && j === cellJ) continue
-            // console.log('gBoard[i][j].minesAroundCount',gBoard[i][j].minesAroundCount)
-            if(gBoard[i][j].minesAroundCount===0) var count = gBoard[i][j].minesAroundCount
-             
-            //  renderCell(location, value) 
-            // gBoard[i][j].minesAroundCount += setMinesNegsCount(i, j, gBoard)
-            // console.log('gBoard[i][j].minesAroundCount', gBoard[i][j].minesAroundCount)
-             renderCell( {i,j}, count)
+            if (board[i][j].isShown) continue
+            var negsCell = document.querySelector(`.cell-${i}-${j}`)
+            // console.log(negsCell.innerText = gBoard[i][j].minesAroundCount, 'negsCell.innerText = gBoard[i][j].minesAroundCount');
+            negsCell.innerText = gBoard[i][j].minesAroundCount
+            cellCount++
+            checkWin()
         }
     }
 }
 
-// function doSomething(e) {
-//     var rightclick;
-//     if (!e) var e = window.event;
-//     if (e.which) rightclick = (e.which == 3);
-//     else if (e.button) rightclick = (e.button == 2);
-//     alert('Rightclick: ' + rightclick); // true or false
-// }
-// function onCeClicked(elCell, cellI, cellJ) {
+function checkWin() {
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[0].length; j++) {
 
-//     if (gBoard[cellI][cellJ] === LIFE) {
-//         // update the model:
-//         gBoard[cellI][cellJ] = SUPER_LIFE
-//         // console.log('gBoard', gBoard)
-//         // update the dom:
-// elCell.innerText = SUPER_LIFE
-//         blowUpNegs(gBoard, cellI, cellJ)
-//     }
-
-// }
+            if (gBoard[i][j] === FLAG || gBoard[i][j] === BOOM) cellCount++
+            // }
+            console.log('cellCount',cellCount)
+            if (cellCount === ((gLevel.size ** 2) - gLevel.mines)) {
+                var msg = 'YOU WIN'
+                openModal(msg)
+            }
+        }
+    }
+}
 
 function gameOver() {
+    gGame.isOn = false
     console.log('game over');
+    var msg = 'Game Over'
+    openModal(msg)
+    clearInterval(myInterval)
 }
 
 function updateMinesAroundCount() {
@@ -162,12 +211,11 @@ function updateMinesAroundCount() {
         for (var j = 0; j < gBoard[0].length; j++) {
             if (gBoard[i][j] === BOOM) return
             gBoard[i][j].minesAroundCount = setMinesNegsCount(i, j, gBoard)
-
         }
     }
-
     renderBoard(gBoard)
 }
+
 function getClassName(location) {
     const cellClass = 'cell-' + location.i + '-' + location.j
     return cellClass
--- js/utils.js
+++ js/utils.js
@@ -2,11 +2,12 @@
 
 
 // location is an object like this - { i: 2, j: 7 }
-function renderCell(location, value) {
-    // Select the elCell and set the value
-    const elCell = document.querySelector(`.cell-${location.i}-${location.j}`)
-    elCell.innerHTML = value
-}
+// function renderCell(location, value) {
+//     // Select the elCell and set the value
+//     const elCell = document.querySelector(`.cell-${location.i}-${location.j}`)
+//     elCell.innerHTML = value
+// }
+var timerInterval
 
 function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
@@ -20,4 +21,26 @@ function getRandomColor() {
     }
     return color
 }
+function timer() {
+    var startTime = Date.now()
 
+    timerInterval = setInterval( () => {
+        var elapsedTime = Date.now() - startTime
+        document.querySelector('.timer').innerText = (
+            elapsedTime / 1000
+        ).toFixed(3)
+    }, 37)
+}
+function openModal(msg) {
+    const elModal = document.querySelector('.modal')
+    const elSpan = elModal.querySelector('.msg')
+    elSpan.innerText = msg
+    elModal.style.display = 'block'
+}
+
+function closeModal() {
+    const elModal = document.querySelector('.modal')
+    elModal.style.display = 'none'
+    clearInterval(timerInterval)
+    onStart()
+}
